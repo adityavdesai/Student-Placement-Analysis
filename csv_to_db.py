8342d7eea4674025fc519ba491fdd89cf4bc113f
rittmang <ritomgupta99@gmail.com>
2020-04-28 00:21:54 +0530

    script to generate names and genders, update csv with that

--- csv_to_db.py
+++ csv_to_db.py
@@ -1,7 +1,32 @@
 from pymongo import MongoClient
 import pandas as pd
+import names
+import random
 
 
 data=pd.read_csv("placement-data.csv")
-for col in data.columns:
-    print(col)
+
+gender=['Male','Female']
+
+fname=[]
+lname=[]
+ngender=[]
+
+def namesGender():
+    g = random.choice(gender)
+    if g=='Male':
+        fname.append(names.get_first_name(gender='male'))
+    if g=='Female':
+        fname.append(names.get_first_name(gender='female'))
+    lname.append(names.get_last_name())
+    ngender.append(g)
+
+
+for i in range(0,20000):
+    namesGender()
+   
+data.insert(0,'FirstName',fname)
+data.insert(1,'LastName',lname)
+data.insert(2,'Gender',ngender)
+print(data.head())
+data.to_csv('placement-data.csv')
