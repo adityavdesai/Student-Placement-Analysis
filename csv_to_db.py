d0403bc810021aef68d476600c43410ae847b63c
rittmang <ritomgupta99@gmail.com>
2020-05-03 19:46:23 +0530

    Add unique email for each student

--- csv_to_db.py
+++ csv_to_db.py
@@ -1,12 +1,31 @@
 from pymongo import MongoClient
 import pandas as pd
-import names
+#import names
 import random
 
 
 data=pd.read_csv("placement-data.csv")
+df=data[data.duplicated(subset=["FirstName","LastName"])]
+#print(df)
+email=[]
 
-gender=['Male','Female']
+
+def getUniqueEmail(i):
+    first=data.iloc[i,1]
+    last=data.iloc[i,2]
+    id=data.iloc[i,0]
+    email.append(str(first.lower()+last.lower()+"."+str(id)+"@email.com"))
+    #print(len(email))
+
+for i in range(0,20000):
+    getUniqueEmail(i)
+    if(i == 1):
+        print(email)
+data.insert(3,'email',email)
+print(data.head())
+data.to_csv('placement-data.csv',index=False)
+
+'''gender=['Male','Female']
 
 fname=[]
 lname=[]
@@ -29,4 +48,5 @@ data.insert(0,'FirstName',fname)
 data.insert(1,'LastName',lname)
 data.insert(2,'Gender',ngender)
 print(data.head())
-data.to_csv('placement-data.csv')
+data.to_csv('placement-data.csv')'''
+
